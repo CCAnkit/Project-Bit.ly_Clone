6e6376ea7c28d4d95fcb402f15d9ed513170f0da
CCAnkit <ankitgupta9828@gmail.com>
2022-04-06 23:35:45 +0530

    Project Completed

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -3,24 +3,52 @@ const express = require('express');
 const validUrl = require('valid-url')
 const shortId = require('shortid')
 const validator = require("../validators/validator.js");
+const redis = require("redis");
+const { promisify } = require("util");
 
 
-const baseUrl = `http://localhost:3000`;      // The API base Url endpoint
+
+
+
+// ----------------Redis Connection-----------------------------------------------------------------------------------
+const redisClient = redis.createClient(         //Connect to the redis server
+    13491,
+    "redis-13491.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("BuMCtOJ6K2GtgEGrtKNmPEPT8rmemIwS", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+      console.log("Connected to Redis..");
+    });
+    
+//Connection setup for redis
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);     //
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);     //
+
+
+
 
 
 // ----------------URL Shorten-----------------------------------------------------------------------------------
 const urlShorten = async function(req, res) {
     try{
+        const baseUrl = `http://localhost:3000`;      // The API base Url endpoint
+        
         const url = req.body
         
         if (!validator.isValidDetails(url)){
             return res.status(401).send({status: false, msg: `Invalid request parameters. Please provide Url details`})
         }
-       
+        
         if (!validUrl.isWebUri(baseUrl)) {     // check the valid format of baseUrl by using the validUrl.isWebUri method
             return res.status(401).send({status: false, msg: 'Base URL is in Invalid format'})
         }
         const { longUrl } = url         // destructure the longUrl from req.body.longUrl
+        
+        // longUrl = longUrl.trim();
 
         let newUrl = {}       //blank Object 
 
@@ -51,6 +79,10 @@ const urlShorten = async function(req, res) {
         newUrl.urlCode = urlCode        //storing the urlCode into the newUrl object
 
         let finalData = await urlModel.create(newUrl)   //creating the ShortUrl   
+
+        await SET_ASYNC(`${urlCode}`,JSON.stringify(longUrl));
+        await SET_ASYNC(`${longUrl}`,JSON.stringify(longUrl));  //setting data in cache -> new entries
+
         return res.status(201).send({status: true, msg: "ShortUrl is created Successfully", data: finalData })
     }
     catch(err) {
@@ -63,12 +95,20 @@ const urlShorten = async function(req, res) {
 const redirectUrl = async function(req, res) {
     try{
         const urlCode = req.params.urlCode
-        const url = await urlModel.findOne({urlCode: urlCode})      //finding the urlCode in urlModel
-        if (url) {     
-            return res.status(301).redirect(url.longUrl)     // if url is valid we perform a redirect
 
-        } else { 
-            return res.status(404).send({status: false, msg: 'No URL Found'})   //If not found     
+        let cachedLongUrl = await GET_ASYNC(urlCode);       //finding data in cache memory by using GET_ASYNC function
+  
+        let parsedcachedLongUrl = JSON.parse(cachedLongUrl)     //changes the data in JSON form 
+
+        if (parsedcachedLongUrl) {
+            return res.status(301).redirect(parsedcachedLongUrl);       
+        } else {
+            const url = await urlModel.findOne({urlCode: urlCode});     //finding the urlCode in urlModel
+            if (url) {
+                return res.status(301).redirect(url.longUrl);
+            } else {
+                return res.status(404).send({ status: false, msg: "No URL Found" });   //If not found  
+            }
         }
     }
     catch(err) {
@@ -76,6 +116,9 @@ const redirectUrl = async function(req, res) {
         return res.status(500).send({msg: err.message})
     }
 }
+
+
+
 
 module.exports.urlShorten = urlShorten;
 module.exports.redirectUrl = redirectUrl;
